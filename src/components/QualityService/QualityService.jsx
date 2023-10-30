6f90f1b0548ebf8fe95172f0ac09588eb9eb9ef3
kev0921 <kevin.hu0921@gmail.com>
2023-10-29 23:34:06 -0400

    added offers to home page

--- src/components/QualityService/QualityService.jsx
+++ src/components/QualityService/QualityService.jsx
@@ -30,11 +30,23 @@ const QualityService = () => {
       <div className={`md:hidden relative overflow-hidden bg-sky-200 transform  transition-transform duration-1500 ${animate ? 'translate-x-0' : '-translate-x-full'}`}>
         <img className="w-full" src={family1} alt="Quality Service" />
         <div className={`absolute inset-0 flex flex-col justify-center px-5 py-12 ext-center md:text-left bg-opacity-75 bg-white`}>
-          <h1 className={`text-3xl text-blue-900 mb-4 text-center`}>
+          <h1 className={`text-4xl text-blue-900 mb-4 text-center`}>
             Trusted, Personalized & Affordable Dental Care
           </h1>
-          <p className={`text-base text-blue-900 leading-relaxed text-center`}>
-            At Orchard Family Dentistry, our caring team is dedicated to providing the highest level of convenience and personalized care for your entire family. We offer a range of modern dentistry services and aim to keep you healthy throughout your life.
+          <p className={`text-lg text-blue-900 leading-relaxed text-center`}>
+            At Orchard Family Dentistry, our caring team is dedicated to providing the highest level of 
+            convenience and personalized care for your entire family. We offer a range of modern dentistry 
+            services and aim to keep you healthy throughout your life.
+          </p>
+          <p className={`pt-5 text-lg text-blue-900 leading-relaxed text-center`}>
+          We believe that affordable dental care should be available to everyone. 
+          Your dental hygiene should never be lessened due to whether or not you have dental insurance. 
+          That’s why we offer cash pricing for cleaning and polishing for only
+            <a href="/offers" className="underline pl-1">$99*!</a>
+          </p>
+
+          <p className={`pt-5 text-sm text-blue-900 leading-relaxed text-center`}>
+          *2 units scaling, 2 Bitewing x-rays, and exam
           </p>
         </div>
       </div>
@@ -52,6 +64,17 @@ const QualityService = () => {
             modern dentistry & we want to do our part to keep you healthy through
             your entire life!
           </p>
+          <p className={`mx-auto w-4/5 text-2xl text-blue-900 pt-10 whitespace-wrap text-center`}>
+          We believe that affordable dental care should be available to everyone. 
+          Your dental hygiene should never be lessened due to whether or not you have dental insurance. 
+          That’s why we offer cash pricing for cleaning and polishing for only
+            <a href="/offers" className="underline pl-1">$99*!</a>
+          </p>
+
+          <p className={`mx-auto w-4/5 text-1xl text-blue-900 pt-2 whitespace-wrap text-center`}>
+          *2 units scaling, 2 Bitewing x-rays, and exam
+          </p>
+
         </div>
         <img className={`md:w-1/4 h-auto w-1/3 object-cover`} src={family1} alt="" />
       </div>
